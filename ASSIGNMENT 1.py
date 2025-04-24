c51b5d70ddcd6067dcda077ffa5af386bfc76685
Jaynil2017 <skk54321@gmail.com>
2025-04-24 18:44:50 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,30 @@
+#Task 1: Perform Basic Mathematical Operations
+a=input('first number:' )
+
+b=input('secand number:')
+
+print('first number',a)
+a=int(a)
+print('2nd number',b)
+b=int(b)
+
+c= a+b
+c=int(c)
+print('sum of two input',c)
+
+d= a*b
+d=int(d)
+print('Multiplication of two:',d)
+
+e=a-b
+print('subt of two number:',e)
+f=a/b
+print('divi of two number:',f)
+
+
+#Task 2: Create a Personalized Greeting
+a=input('enter your first name:')
+b=input('enter your lastname:')
+c=a+b
+print('hello',c)
+print('welcome to skk python class')
---
+++ Task 2.py
@@ -0,0 +1,5 @@
+a=input('enter your first name:')
+b=input('enter your lastname:')
+c=a+' '+b
+print('hello',c)
+print('welcome to skk python class')
