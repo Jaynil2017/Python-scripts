cb2cb3c6c55d84357483d7192879443dacb1d1e0
Jaynil2017 <skk54321@gmail.com>
2025-04-25 20:58:54 +0530

    Add files via upload

---
+++ ASSIGNMENT 2.py
@@ -0,0 +1,18 @@
+a=int(input('enter your number:'))
+
+b=a%2
+if b == 0:
+    print('this is even number')
+else:
+    print('this is odd number')
+print('thank you')
+
+#sum of 1 to 50 
+
+sum = 0
+for i in range(1,51):
+    sum+=i
+    print('sum of 1 to 50',sum)
+    
+
+   
